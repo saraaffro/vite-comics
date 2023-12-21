4ff292255c3c7dd4c96765ad223a7973b36ee570
saraaffro <saraaffro@gmail.com>
2023-12-21 14:13:15 +0100

    create: header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<template>
+    <header>
+        <div class="logo-container">
+            <img src="../../public/img/dc-logo.png" alt="logo">
+        </div>
+        <ul>
+            <li><a href="#">testo</a></li>
+            <li><a href="#">testo</a></li>
+            <li><a href="#">testo</a></li>
+            <li><a href="#">testo</a></li>
+            <li><a href="#">testo</a></li>
+            <li><a href="#">testo</a></li>
+            <li><a href="#">testo</a></li>
+            <li><a href="#">testo</a></li>
+            <li><a href="#">testo</a></li>
+            <li><a href="#">testo</a></li>
+        </ul>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
+
+header {
+    @include flex;
+    height: 100px;
+    width: 100%;
+
+    .logo-container {
+        max-width: 80px;
+
+        img {
+            max-width: 100%;
+        }
+    }
+
+    ul {
+        list-style-type: none;
+        @include flex;
+
+
+        li a {
+            color: black;
+            text-decoration: none;
+            padding: 20px 10px;
+
+            &:hover,
+            &.active {
+                color: $main-color;
+            }
+
+            &.active {
+                border-bottom: 3px solid $main-color;
+            }
+        }
+
+
+    }
+}
+</style>
